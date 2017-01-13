b27ef53af5923a3b015177ac222496adbe371cf4
liushuai0620 <763779230@qq.com>
2017-01-13 10:55:53 +0800

    first setting

--- app/src/main/java/com/example/administrator/liushuaiapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/liushuaiapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.liushuaiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //修改一次
+        Log.i("<<<", "onCreate: this is first setting");
     }
 }
